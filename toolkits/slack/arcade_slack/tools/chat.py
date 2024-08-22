b4008ac97c9ebf8d30854c71a1c6e62808d930b5
Nate Barbettini <nate@arcade-ai.com>
2024-08-22 14:42:07 -0700

    Add RetryableToolError

--- toolkits/slack/arcade_slack/tools/chat.py
+++ toolkits/slack/arcade_slack/tools/chat.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from arcade.core.errors import ToolExecutionError, RetryableToolError
 from arcade.core.schema import ToolContext
 from arcade.sdk import tool
 from arcade.sdk.auth import SlackUser
@@ -22,9 +23,9 @@ def send_dm_to_user(
 
     try:
         # Step 1: Retrieve the user's Slack ID based on their username
-        response = slackClient.users_list()
+        userListResponse = slackClient.users_list()
         user_id = None
-        for user in response["members"]:
+        for user in userListResponse["members"]:
             if user["name"].lower() == user_name.lower():
                 user_id = user["id"]
                 break
@@ -33,7 +34,11 @@ def send_dm_to_user(
             # does this end up as a developerMessage?
             # does it end up in the LLM context?
             # provide the dev an Error type that controls what ends up in the LLM context
-            raise ValueError(f"User with username '{user_name}' not found.")
+            raise RetryableToolError(
+                "User not found",
+                developer_message=f"User with username '{user_name}' not found.",
+                additional_prompt_content=format_users(userListResponse),
+            )
 
         # Step 2: Retrieve the DM channel ID with the user
         im_response = slackClient.conversations_open(users=[user_id])
@@ -43,5 +48,18 @@ def send_dm_to_user(
         slackClient.chat_postMessage(channel=dm_channel_id, text=message)
 
     except SlackApiError as e:
-        # this should be caught also, not printed
-        print(f"Error sending message: {e.response['error']}")
+        raise ToolExecutionError(
+            f"Error sending message: {e.response['error']}",
+            developer_message="Error sending message",
+        )
+
+
+def format_users(userListResponse: dict) -> str:
+    csv_string = "All active Slack users:\n\nid,name,real_name\n"
+    for user in userListResponse["members"]:
+        if not user.get("deleted", False):
+            user_id = user.get("id", "")
+            name = user.get("name", "")
+            real_name = user.get("profile", {}).get("real_name", "")
+            csv_string += f"{user_id},{name},{real_name}\n"
+    return csv_string.strip()
