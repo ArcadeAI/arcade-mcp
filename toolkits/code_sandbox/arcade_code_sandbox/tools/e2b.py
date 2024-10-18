29446b6f50e5b6318fcecffb781dc5890ee8cd22
Eric Gustin <eric@arcade-ai.com>
2024-10-17 18:07:59 -0700

    WIP chart creation

--- toolkits/code_sandbox/arcade_code_sandbox/tools/e2b.py
+++ toolkits/code_sandbox/arcade_code_sandbox/tools/e2b.py
@@ -1,3 +1,4 @@
+import base64
 import os
 from typing import Annotated, Any, Optional
 
@@ -27,6 +28,28 @@ def run_code(
     sbx = Sandbox(api_key=api_key)  # By default the sandbox is alive for 5 minutes
     execution = sbx.run_code(code=code, language=language)  # Execute Python inside the sandbox
     sbx.kill()
-    print(execution.logs)
 
-    return execution.text
+    return str(execution)
+
+
+@tool
+def create_static_matplotlib_charts(
+    code: Annotated[str, "The code to run"],
+    language: Annotated[str, "The language of the code"] = "python",
+) -> Annotated[str, "The text representation of the executed code's output"]:
+    """
+    Run the provided code to generate static matplotlib chart(s). The resulting charts are converted to images, encoded in base64 format, and then decoded into a string for return.
+    """
+    api_key = get_secret("E2B_API_KEY")
+    sbx = Sandbox(api_key=api_key)  # By default the sandbox is alive for 5 minutes
+    execution = sbx.run_code(code=code, language=language)  # Execute Python inside the sandbox
+    # There's only one result in this case - the plot displayed with `plt.show()`
+    first_result = execution.results[0]
+
+    if first_result.png:
+        # Save the png to a file. The png is in base64 format.
+        with open("chart.png", "wb") as f:
+            f.write(base64.b64decode(first_result.png))
+        print("Chart saved as chart.png")
+
+    return "Successfully created static matplotlib chart(s) and saved as chart.png"
