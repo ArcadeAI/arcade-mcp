7e5f049ca0c3c793e584caa98f5e90031169cf97
Eric Gustin <eric@arcade-ai.com>
2024-11-08 16:28:00 -0800

    Update code sandbox tools

--- toolkits/code_sandbox/arcade_code_sandbox/tools/utils.py
+++ toolkits/code_sandbox/arcade_code_sandbox/tools/utils.py
@@ -4,8 +4,6 @@ from typing import Any, Optional
 
 def get_secret(name: str, default: Optional[Any] = None) -> Any:
     secret = os.getenv(name)
-    if secret is None:
-        if default is not None:
-            return default
-        raise ValueError(f"Secret {name} is not set.")
+    if secret is None and default is not None:
+        return default
     return secret
