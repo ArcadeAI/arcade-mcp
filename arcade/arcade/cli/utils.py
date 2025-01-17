722005bed2f0756b8b28ff871444f98e0e10fe54
Eric Gustin <eric@arcade-ai.com>
2025-01-17 09:24:03 -0800

    More arcadepy compatibility

--- arcade/arcade/cli/utils.py
+++ arcade/arcade/cli/utils.py
@@ -412,8 +412,8 @@ def handle_tool_authorization(
     stream: bool,
 ) -> ChatInteractionResult:
     with Live(console=console, refresh_per_second=4) as live:
-        if tool_authorization.url:  # type: ignore[attr-defined]
-            authorization_url = str(tool_authorization.url)  # type: ignore[attr-defined]
+        if tool_authorization.url:
+            authorization_url = str(tool_authorization.url)
             webbrowser.open(authorization_url)
             message = (
                 "You'll need to authorize this action in your browser.\n\n"
@@ -446,8 +446,7 @@ def wait_for_authorization_completion(
     while auth_response.status != "completed":
         try:
             auth_response = client.auth.status(
-                authorization_id=cast(str, auth_response.id),  # type: ignore[attr-defined]
-                scopes=" ".join(auth_response.scopes) if auth_response.scopes else NOT_GIVEN,
+                id=cast(str, auth_response.id),
                 wait=59,
             )
         except APITimeoutError:
