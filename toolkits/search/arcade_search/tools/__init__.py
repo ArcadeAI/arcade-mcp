f89b9d72c515823e752d722cc607e77ff8500c3c
Eric Gustin <eric@arcade.dev>
2025-03-11 20:36:11 -0700

    Add flights tools to root of tools init

--- toolkits/search/arcade_search/tools/__init__.py
+++ toolkits/search/arcade_search/tools/__init__.py
@@ -1,8 +1,11 @@
 from arcade_search.tools.google_finance import get_stock_chart_data, get_stock_summary
+from arcade_search.tools.google_flights import search_one_way_flights, search_roundtrip_flights
 from arcade_search.tools.google_search import search_google
 
 __all__ = [
     "search_google",  # Google Search
     "get_stock_summary",  # Google Finance
     "get_stock_chart_data",  # Google Finance
+    "search_one_way_flights",  # Google Flights
+    "search_roundtrip_flights",  # Google Flights
 ]
