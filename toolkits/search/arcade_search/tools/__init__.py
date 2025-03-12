66522a19100e0e2dba3c82bf56f1594f73b1bb3d
Eric Gustin <eric@arcade.dev>
2025-03-11 18:10:33 -0700

    Finish Google Finance Tools

---
+++ toolkits/search/arcade_search/tools/__init__.py
@@ -0,0 +1,8 @@
+from arcade_search.tools.google_finance import get_stock_chart_data, get_stock_summary
+from arcade_search.tools.google_search import search_google
+
+__all__ = [
+    "search_google",  # Google Search
+    "get_stock_summary",  # Google Finance
+    "get_stock_chart_data",  # Google Finance
+]
