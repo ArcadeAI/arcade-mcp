3be33b59094dd41d053828bd6358d68a50b272ab
Eric Gustin <eric@arcade.dev>
2025-07-02 10:57:31 -0700

    Add GoogleDocs

---
+++ toolkits/google_docs/arcade_google_docs/tools/__init__.py
@@ -0,0 +1,13 @@
+from arcade_google_docs.tools.docs import (
+    create_blank_document,
+    create_document_from_text,
+    get_document_by_id,
+    insert_text_at_end_of_document,
+)
+
+__all__ = [
+    "create_blank_document",
+    "create_document_from_text",
+    "get_document_by_id",
+    "insert_text_at_end_of_document",
+]
---
+++ toolkits/google_docs/arcade_google_docs/utils.py
@@ -0,0 +1,10 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import Resource, build
+
+
+def build_docs_service(auth_token: str | None) -> Resource:  # type: ignore[no-any-unimported]
+    """
+    Build a Drive service object.
+    """
+    auth_token = auth_token or ""
+    return build("docs", "v1", credentials=Credentials(auth_token))
