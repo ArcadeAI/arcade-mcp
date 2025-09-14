9a2c5b6a631580a8ea6a3c2849a269a29666eee4
Ray Smets <rayjsmets@gmail.com>
2025-09-14 13:10:20 -0700

    [Mastra] gmail agent example added back as was

--- examples/mastra/src/mastra/index.ts
+++ examples/mastra/src/mastra/index.ts
@@ -1,9 +1,10 @@
 import { Mastra } from "@mastra/core"
 import { LibSQLStore } from "@mastra/libsql"
-import { inboxTravelSearchAgent } from "./agents/inboxTravelSearchAgent"
+import { inboxTravelSearchAgent } from "./agents/inboxTravelSearch"
+import { gmailAgent } from "./agents/gmail"
 
 export const mastra = new Mastra({
-    agents: { inboxTravelSearchAgent },
+    agents: { gmailAgent, inboxTravelSearchAgent },
     storage: new LibSQLStore({
         url: "file:../mastra.db",
     }),
