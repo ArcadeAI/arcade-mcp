a921b76ce963cb11eba5cf6415566b09692d9c95
Evan Tahler <evantahler@gmail.com>
2025-11-21 15:53:54 -0800

    Add tool name and version to otel spans for worker (#690)
    Knowing which tools we called seems important
    
    <img width="1554" height="293" alt="Screenshot 2025-11-20 at 1 36 48 PM"
    src="https://github.com/user-attachments/assets/1b3cf297-749f-4ea1-9d1c-606707540d5b"
    />
    
    Closes PLT-748

--- libs/arcade-serve/arcade_serve/core/components.py
+++ libs/arcade-serve/arcade_serve/core/components.py
@@ -66,9 +66,15 @@ class CallToolComponent(WorkerComponent):
         Handle the request to call (invoke) a tool.
         """
         tracer = trace.get_tracer(__name__)
-        with tracer.start_as_current_span("CallTool"):
+        with tracer.start_as_current_span("CallTool") as current_span:
             call_tool_request_data = request.body_json
             call_tool_request = ToolCallRequest.model_validate(call_tool_request_data)
+
+            current_span.set_attribute("tool_name", str(call_tool_request.tool.name))
+            current_span.set_attribute("toolkit_version", str(call_tool_request.tool.version))
+            current_span.set_attribute("toolkit_name", str(call_tool_request.tool.toolkit))
+            current_span.set_attribute("environment", self.worker.environment)
+
             return await self.worker.call_tool(call_tool_request)
 
 
