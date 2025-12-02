83ec80c08f057497e245cbb0402286a8a9f37a6a
Evan Tahler <evantahler@gmail.com>
2025-12-01 18:38:22 -0800

    Bumps critical JS dependency versions (#699)
    Fixes a number of security issues with JS dependencies in our examples.
    Does not upgrade to the latest versions of frameworks.

--- contrib/examples/mastra/src/mastra/tools/gmailTools.ts
+++ contrib/examples/mastra/src/mastra/tools/gmailTools.ts
@@ -25,9 +25,20 @@ const gmailToolkit = await arcade.tools.list({ toolkit: "Gmail", limit: 30 })
  *
  * Learn more: https://docs.arcade.dev/home/use-tools/get-tool-definitions#get-zod-tool-definitions
  */
-export const gmailTools = toZodToolSet({
+const baseGmailTools = toZodToolSet({
     tools: gmailToolkit.items,
     client: arcade,
     userId: "<YOUR_USER_ID>", // Your app's internal ID for the user (an email, UUID, etc). It's used internally to identify your user in Arcade
     executeFactory: executeOrAuthorizeZodTool, // Checks if tool is authorized and executes it, or returns authorization URL if needed
 })
+
+// Add id property to each tool that copies the name
+export const gmailTools = Object.fromEntries(
+    Object.entries(baseGmailTools).map(([key, tool]) => [
+        key,
+        {
+            ...tool,
+            id: tool.name,
+        },
+    ])
+)
